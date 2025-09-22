4581f13a074568dd79715600f9a02c8a73aafdbb
aaryaa4 <aryamore04@gmail.com>
2025-09-22 20:46:37 +0530

    Fix SQLAlchemy URL format

--- database.py
+++ database.py
@@ -3,14 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This now reads the database URL from the Railway environment variable
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# This checks for the 'postgres://' protocol and fixes it for SQLAlchemy
+# This is the line that fixes the URL format
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# The connect_args is removed as it's only for SQLite
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
