591eb04c36d05ec13a8731fa52c41aab339fdc0d
aaryaa4 <aryamore04@gmail.com>
2025-09-22 20:07:24 +0530

    Configure app for PostgreSQL database

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./civic_connect.db"
+# This now reads the database URL from the Railway environment variable
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# The connect_args is removed as it's only for SQLite
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -14,6 +16,6 @@ Base = declarative_base()
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+       yield db
     finally:
         db.close()
